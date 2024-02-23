aaee2a328d2d23d1aa512b75417e9ea9f1df212f
NandhaScorpio <gokulamstore2010@gmail.com>
2024-02-23 17:00:34 +0530

    Updated with many styles

---
+++ index.js
@@ -0,0 +1,9 @@
+var sidenav = document.querySelector(".side-categories-popup")
+function openNavbar()
+{
+    sidenav.style.left = "0"
+}
+function closeNavbar()
+{
+    sidenav.style.left = "-60%"
+}
